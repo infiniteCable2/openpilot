503cdd37c97ff7d83e9440fbb3e3033555b08632
infiniteCable2 <steffen.dahansi.werner@googlemail.com>
2024-10-24 20:14:28 +0200

    Update interface.py tuning

--- selfdrive/car/volkswagen/interface.py
+++ selfdrive/car/volkswagen/interface.py
@@ -107,8 +107,8 @@ class CarInterface(CarInterfaceBase):
       ret.radarTimeStep = (1.0 / 25) # 25Hz
       #ret.longitudinalTuning.kpBP = [0., 5., 35.]
       #ret.longitudinalTuning.kpV  = [0., 0.1, 0.4]
-      ret.longitudinalTuning.kiBP = [0., 5., 15.]
-      ret.longitudinalTuning.kiV  = [0.3, 0.2, 0.]
+      ret.longitudinalTuning.kiBP = [0., 3., 6.]
+      ret.longitudinalTuning.kiV  = [0.8, 0.4, 0.]
 
     ret.experimentalLongitudinalAvailable = ret.networkLocation == NetworkLocation.gateway or docs
     if experimental_long:
