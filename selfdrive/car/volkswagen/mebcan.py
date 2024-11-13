e9b753f735f3916db565baf35f08a6c026caa6a2
infiniteCable2 <steffen.dahansi.werner@googlemail.com>
2024-11-13 18:00:34 +0100

    Update mebcan.py

--- selfdrive/car/volkswagen/mebcan.py
+++ selfdrive/car/volkswagen/mebcan.py
@@ -15,7 +15,7 @@ ACC_HUD_ENABLED  = 2
 ACC_HUD_DISABLED = 0
 
 
-def create_steering_control(packer, bus, apply_curvature, lkas_enabled, power, counter):
+def create_steering_control(packer, bus, apply_curvature, lkas_enabled, power):
   # active lateral control deactivates active steering wheel centering 
   signal_test = f'NEW_SIGNAL_{counter}'
   values = {
@@ -26,7 +26,6 @@ def create_steering_control(packer, bus, apply_curvature, lkas_enabled, power, c
     "Active": lkas_enabled,
     "Request": lkas_enabled,
     "Standby": not lkas_enabled,
-    signal_test: 1,
   }
   return packer.make_can_msg("HCA_03", bus, values)
 
